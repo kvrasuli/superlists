dc3f6cd5a21459649b5c9ead7f1eb402f6a20ec3
Kirill Rasuli <rasulikv@gmail.com>
2020-02-28 22:40:39 +0300

    Рефакторизованы представления домашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
@@ -11,13 +12,24 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		'''тест: домашняя страница возвращает правильный html'''
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+	# def test_home_page_returns_correct_html(self):
+	# 	'''тест: домашняя страница возвращает правильный html'''
+	# 	# request = HttpRequest()
+	# 	# response = home_page(request)
+	# 	# html = response.content.decode('utf8')
+	# 	# expected_html = render_to_string('home.html')
+	# 	# self.assertEqual(html, expected_html)
+	# 	response = self.client.get('/')
 
+	# 	html = response.content.decode('utf8')
+	# 	self.assertTrue(html.startswith('<html>'))
+	# 	self.assertIn('<title>To-Do lists</title>', html)
+	# 	self.assertTrue(html.strip().endswith('</html>'))
+
+	# 	self.assertTemplateUsed(response, 'home.html')
+
+	def test_uses_home_template(self):
+		'''тест домашней страницы'''
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
 	
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home_page(request):
 	'''домашняя страница'''
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return render(request, 'home.html')
 
 
