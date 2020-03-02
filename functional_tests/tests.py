4c2d11f46d4491958984e31be300a9867a0e1ff5
Kirill Rasuli <rasulikv@gmail.com>
2020-03-02 23:21:23 +0300

    Первые шаги ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,6 +30,24 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 
+	def test_layout_and_styling(self):
+		'''тест макета и стилевого оформления'''
+		# эдит открывает домашнюю страницу
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		#она замечает, что поле ввода аккуратно центрировано
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+		#она начинает новый список и видит что поле ввода там тоже аккуратно центрировано
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+		
+
 	def test_can_start_a_list_for_one_user(self):
 		'''тест: можно начать список для одного пользователя'''
 		#эдит слышала про приложение с онлайн списком по адресу
@@ -116,3 +134,5 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Купить молоко', page_text)
 
 		#удовлетворенные они ложаться спать (you know what i mean)
+
+	
