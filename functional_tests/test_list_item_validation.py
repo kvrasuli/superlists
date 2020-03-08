7020c121e6e2ccefb93d30b445a4e0a3d6b25a0a
Kirill Rasuli <rasulikv@gmail.com>
2020-03-08 15:05:43 +0300

    Обеспечена валидация модели в представлении списка

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -19,9 +19,9 @@ class ItemValidationTest(FunctionalTest):
 		))
 
 		#она пробует снова, теперь с неким текстом для элемента, и теперь это срабатывает
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy Milk')
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy Milk')
+		self.wait_for_row_in_list_table('1: Buy milk')
 
 		#как ни странно, эдит решает отправить второй пустой элемент списка
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -64,8 +64,20 @@ class ListViewTest(TestCase):
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new item for an existing list')
 		self.assertEqual(new_item.list, correct_list)
-	
-	def test_redirects_to_list_view(self):
+
+	def test_validation_errors_end_up_on_lists_page(self):
+		'''тест: ошибки валидации оканчиваются на странице списков'''
+		list_ = List.objects.create()
+		response = self.client.post(
+			f'/lists/{list_.id}/',
+			data = {'item_text': ''}
+		)
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'list.html')
+		expected_error = escape("You can't have an empty list item")
+		self.assertContains(response, expected_error)
+
+	def test_POST_redirects_to_list_view(self):
 		'''тест: переадресуется в представление списка'''
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
@@ -102,8 +114,7 @@ class NewListTest(TestCase):
 
 	def test_invalid_list_items_arent_saved(self):
 		'''тест: сохраняются недопусимые элементы списка'''
-		self.client.post('/list/new', data={'item_text': ''})
+		self.client.post('/lists/new', data={'item_text': ''})
 		self.assertEqual(List.objects.count(), 0)
 		self.assertEqual(Item.objects.count(), 0)
-
 
