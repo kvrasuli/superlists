5d6f0dbdd0aec68119c0275fe329c36b15f16546
Kirill Rasuli <rasulikv@gmail.com>
2020-03-11 20:48:52 +0300

    Первая "примерка" нового ФТ для исчезновения сообщений об ошибках

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,56 +1,81 @@
 from selenium.webdriver.common.keys import Keys
 from unittest import skip
-from .base import  FunctionalTest
+from .base import FunctionalTest
+
 
 class ItemValidationTest(FunctionalTest):
-	'''тест валидации элемента списка'''
-	def test_cannot_add_empty_list_items(self):
-		'''тест: нельзя добавлять пустые элементы списка'''
-		# эдит открывает домашнюю страницу и случайно пытается отправить
-		# пустой элемент списка. Она нажимает Enter на пустом поле ввода
-		self.browser.get(self.live_server_url)
-		self.get_item_input_box().send_keys(Keys.ENTER)
+    '''тест валидации элемента списка'''
+    def get_error_element(self):
+        '''получить элемент с ошибкой'''
+        return self.browser.find_element_by_css_selector('.has-error')
 
-		#браузер перехватывает запрос и не загружает страницу со списком
+    def test_cannot_add_empty_list_items(self):
+        '''тест: нельзя добавлять пустые элементы списка'''
+        # эдит открывает домашнюю страницу и случайно пытается отправить
+        # пустой элемент списка. Она нажимает Enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-		#домашняя страница  обновляется, и появляется сообщение об ошибке,
-		#которое говорит, что элементы списка не должны быть пустыми
-		self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
+        # браузер перехватывает запрос и не загружает страницу со списком
 
-		#эдит начинает набирать текст нового элемента и ошибка исчезает
-		self.get_item_input_box().send_keys('Buy milk')
-		self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
+        # домашняя страница  обновляется, и появляется сообщение об ошибке,
+        # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
 
-		#как ни странно, эдит решает отправить второй пустой элемент списка
-		self.get_item_input_box().send_keys(Keys.ENTER)
+        # эдит начинает набирать текст нового элемента и ошибка исчезает
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
 
-		#и снова браузер не подчинился
-		self.wait_for_row_in_list_table('1: Buy milk')
-		self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
-		
-		#и она может его исправить, заполнив поле неким текстом
-		self.get_item_input_box().send_keys('Make tea')
-		self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy milk')
-		self.wait_for_row_in_list_table('2: Make tea')
+        # как ни странно, эдит решает отправить второй пустой элемент списка
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-	def test_cannot_add_duplicate_items(self):
-		'''тест: нельзя добавлять повторяющиеся элементы'''
-		#эдит открывают домашнюю страницу и начинает новый список
-		self.browser.get(self.live_server_url)
-		self.get_item_input_box().send_keys('Buy wellies')
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy wellies')
+        # и снова браузер не подчинился
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
 
-		#она случайно пытается ввести повторяющийся элемент
-		self.get_item_input_box().send_keys('Buy wellies')
-		self.get_item_input_box().send_keys(Keys.ENTER)
-		
-		#она видит полезное сообщение об ошибке
-		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text, 
-			"You've already got this in your list"
-			))
+        # и она может его исправить, заполнив поле неким текстом
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
-		
+    def test_cannot_add_duplicate_items(self):
+        '''тест: нельзя добавлять повторяющиеся элементы'''
+        # эдит открывают домашнюю страницу и начинает новый список
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # она случайно пытается ввести повторяющийся элемент
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # она видит полезное сообщение об ошибке
+        self.wait_for(lambda: self.assertEqual(
+            self.get_error_element().text,
+            "You've already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        '''тест: сообщения об ошибках очищаются при вводе'''
+        # эдит начинает список и вызывает ошибку валидации
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # она начинает набирать в поле ввода
+        self.get_item_input_box().send_keys('a')
+
+        # она довольна что сообщение об ошибке исчезает
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
