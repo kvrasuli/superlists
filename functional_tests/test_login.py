9e1a7fc9d514aeb81dd82967c3ee84b5d35f4566
Kirill Rasuli <rasulikv@gmail.com>
2020-03-21 21:01:18 +0300

    test_my_lists: предсоздание сеансов, перемещение проверок регистрации в базу

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -41,14 +41,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # она зарегистрирована в системе!
-        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         #теперь она выходит из системы
         self.browser.find_element_by_link_text('Log out').click()
 
         #она вышла из системы
-        self.wait_for(lambda: self.browser.find_element_by_name('email'))
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
